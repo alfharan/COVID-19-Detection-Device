a9183a1618c57cc5e4f8205840478a41da2a40d8
alfharan <59139641+alfharan@users.noreply.github.com>
2020-03-21 12:52:55 +0800

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,71 @@
+# Created by:
+# Name : Alan Fhajoeng Ramadhan
+# From : Indonesia
+# email : alfhatech.id@gmail.com
+# 21 March 2020
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2 as cv
+import random
+import pickle
+
+DATADIR = r'E:\COVID-19 Detection\training data'
+CATEGORIES = ["covid", "normal"]
+
+for category in CATEGORIES:
+    path = os.path.join(DATADIR, category) # path to CATEGORIES
+    for img in os.listdir(path):
+        img_array = cv.imread(os.path.join(path,img), cv.IMREAD_COLOR)
+        plt.imshow(img_array)
+        plt.show()
+        break
+    break 
+print(img_array.shape)
+IMG_SIZE = 500
+
+new_array = cv.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array,cmap ='gray')
+plt.show()
+
+training_data =[]
+
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category) # path to CATEGORIES
+        class_num = CATEGORIES.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv.imread(os.path.join(path,img), cv.IMREAD_GRAYSCALE)
+                new_array = cv.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array, class_num])
+            except Exception as e:
+                pass
+            
+create_training_data()
+
+print(len(training_data))
+
+random.shuffle(training_data)
+for sample in training_data[:10]:
+    print(sample[1])
+X = []
+y = []
+
+for features, label in training_data:
+    X.append(features)
+    y.append(label)
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+pickle_out =open("X.pickle", "wb")
+pickle.dump(X,pickle_out)
+pickle_out.close()
+
+pickle_out =open("y.pickle", "wb")
+pickle.dump(y,pickle_out)
+pickle_out.close()
+
+pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
+
+X[1]
