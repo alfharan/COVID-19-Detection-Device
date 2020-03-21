082cdcfb8b5aeea8963cedf97a1f174e18fbb12d
alfharan <59139641+alfharan@users.noreply.github.com>
2020-03-21 12:13:20 +0800

    Add files via upload

---
+++ test_data.py
@@ -0,0 +1,44 @@
+# Created by:
+# Name : Alan Fhajoeng Ramadhan
+# From : Indonesia
+# email : alfhatech.id@gmail.com
+# 21 March 2020
+
+import cv2
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+CATEGORIES = ["covid", "normal"]
+def prepare(filepath):
+    IMG_SIZE = 500
+    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+model = tf.keras.models.load_model("32x2x0-CNN.model")
+
+image = cv2.imread('1.jpg', cv2.IMREAD_COLOR)
+image = cv2.resize(image, (1500,1500))
+img_array = cv2.imread('1.jpg', cv2.IMREAD_COLOR)
+img_array = cv2.resize(img_array, (1500,1500))
+
+prediction = model.predict([prepare('1.jpg')])
+label = str(CATEGORIES[int(prediction[0][0])])
+if label == 'covid':
+    cv2.putText(img_array, "Suspect : Positive COVID-19", (50,300),  cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,0), 5, cv2.LINE_AA)
+
+else:
+    cv2.putText(img_array, "Suspect : Normal", (50,300),  cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,0), 5, cv2.LINE_AA)
+
+plt.imshow(image)
+plt.show()
+plt.imshow(img_array)
+plt.show()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
---
+++ train_data.py
@@ -0,0 +1,67 @@
+# Created by:
+# Name : Alan Fhajoeng Ramadhan
+# From : Indonesia
+# email : alfhatech.id@gmail.com
+# 21 March 2020
+# usage : python train_data.py
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
+import pickle
+import time
+
+
+pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
+pickle_in = open("y.pickle", "rb")
+y = pickle.load(pickle_in)
+
+X = X / 255.0
+
+dense_layers = [0]
+layer_sizes = [32]
+conv_layers = [2]
+
+for dense_layer in dense_layers:
+   for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            tensorboard = TensorBoard(log_dir= r'E:\COVID-19 Detection\logs\{}'.format(NAME))
+            print(NAME)
+            model = Sequential()
+
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+                model.add(Dropout(0.2))
+
+            model.add(Flatten())
+            for j in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
+                model.add(Dropout(0.2))
+
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+
+            model.compile(loss='binary_crossentropy',
+                        optimizer='adam',
+                        metrics=['accuracy'])
+
+            model.fit(X, y, batch_size=32, epochs=20, validation_split=0.2, callbacks=[tensorboard])
+            
+model.save('32x2x0-CNN.model')
+
+
+
+
+
+
+
